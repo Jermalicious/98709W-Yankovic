d11c96a8453d530b11da19d261c9550259c8a9f2
katiefern@protonmail.com <katiefern@protonmail.com>
2023-11-06 16:30:29 -0600

    Made cata, and modifed auton skills start action

--- include/autonFunctions.h
+++ include/autonFunctions.h
@@ -1,143 +1,144 @@
-// // #include <fstream>
-// // #include <iostream>
+// #include <fstream>
+// #include <iostream>
 
 
-// //define miscellaneous motors, pneumatics, and tracking wheels 
-// 	pros::Motor left_intake(7,true);			//the left intake motor
-// 	pros::Motor right_intake(5,false); 		//the right intake motor
-// 	pros::ADIDigitalOut wings (1, LOW); 	//the pneumatics to extend the pusher wings
-// 	pros::Rotation tracking_wheel_X (13, false); //Change Port
-// 	pros::Rotation tracking_wheel_Y	(12, false); //Change Port
-// 	pros::Imu inertial_sensor (18);
+//define miscellaneous motors, pneumatics, and tracking wheels 
+	pros::Motor left_intake(7,true);		//the left intake motor
+	pros::Motor right_intake(??,false); 	//(not plugged in) the right intake motor
+    pros::Motor cata_motor (5,false);       //the catapult motor
+	pros::ADIDigitalOut wings (1, LOW); 	//the pneumatics to extend the pusher wings
+	pros::Rotation tracking_wheel_X (13, false); //Change Port
+	pros::Rotation tracking_wheel_Y	(12, false); //Change Port
+	pros::Imu inertial_sensor (18);
 
-// //define drivetrain motors
-// 	pros::Motor left_top_drive (2,true);
-// 	pros::Motor left_back_drive (3,false);
-// 	pros::Motor left_front_drive (4, true);
-// 	pros::Motor right_top_drive (10,false);
-// 	pros::Motor	right_back_drive (9,true);
-// 	pros::Motor right_front_drive (8,false);
+//define drivetrain motors
+	pros::Motor left_top_drive (2,true);
+	pros::Motor left_back_drive (3,false);
+	pros::Motor left_front_drive (4, true);
+	pros::Motor right_top_drive (10,false);
+	pros::Motor	right_back_drive (9,true);
+	pros::Motor right_front_drive (8,false);
 
-// //define drivetrain motor groups
-// 	pros::Motor_Group left_drivetrain({left_top_drive, left_back_drive, left_front_drive}); 	//the three motors for the left side of the drivetrain
-// 	pros::Motor_Group right_drivetrain({right_top_drive, right_back_drive, right_front_drive}); //the three motors for the right side of the drivetrain
-// 	pros::Motor_Group intake({left_intake, right_intake});
+//define drivetrain motor groups
+	pros::Motor_Group left_drivetrain({left_top_drive, left_back_drive, left_front_drive}); 	//the three motors for the left side of the drivetrain
+	pros::Motor_Group right_drivetrain({right_top_drive, right_back_drive, right_front_drive}); //the three motors for the right side of the drivetrain
+	pros::Motor_Group intake({left_intake, right_intake});
 
 
 
-// class Controll
-//     {
+class Controll
+    {
     
-//     public:
-//         Controll();
+    public:
+        Controll();
 
-//     protected:
+    protected:
 
-//         float basicPID(double kP, double kI, double kD, float sensor_input, float target, float kI_start_at_error_value, float timeout_msec);
+        float basicPID(double kP, double kI, double kD, float sensor_input, float target, float kI_start_at_error_value, float timeout_msec);
 
-//         double kP;
-//         double kI;
-//         double kD;
+        double kP;
+        double kI;
+        double kD;
 
-//     private:
-//         int timer;
+    private:
+        int timer;
 
-//     };
+    };
 
-// class PIDForward : public Controll
-//     {
-//     public:
+class PIDForward : public Controll
+    {
+    public:
 
-//         PIDForward(double input_kP, double input_kI, double input_kD) //PIDForward constructor. Sets kP, kI, and kD values from an input. Later input from a file?
-//         {
-//             kP = input_kP;
-//             kI = input_kI;
-//             kD = input_kD;
-//         }
+        PIDForward(double input_kP, double input_kI, double input_kD) //PIDForward constructor. Sets kP, kI, and kD values from an input. Later input from a file?
+        {
+            kP = input_kP;
+            kI = input_kI;
+            kD = input_kD;
+        }
 
-//         void run(float target_inches, float kI_start_at_error_value = 10, float timeout_msec = 3000) //Add settle time. This returns an array with two items: left_speed and right_speed
-//         {
-//             float sensor_input = tracking_wheel_Y.get_angle() / 360 * 10.2 /* tracking wheel circumference*/;
+        void run(float target_inches, float kI_start_at_error_value = 10, float timeout_msec = 3000) //Add settle time. This returns an array with two items: left_speed and right_speed
+        {
+            float sensor_input = tracking_wheel_Y.get_angle() / 360 * 10.2 /* tracking wheel circumference*/;
 
-//             float forward_speed = basicPID(kP, kI, kD, sensor_input, target_inches, kI_start_at_error_value, timeout_msec);  
+            float forward_speed = basicPID(kP, kI, kD, sensor_input, target_inches, kI_start_at_error_value, timeout_msec);  
 
-//             left_drivetrain, right_drivetrain = forward_speed; //sets both left and right drivetrain sides to the output of the PID
+            left_drivetrain, right_drivetrain = forward_speed; //sets both left and right drivetrain sides to the output of the PID
 
-//         }
+        }
 
-//     };
-
-
-// class PIDTurn : public Controll
-//     {
-//     public:
-
-//         PIDTurn(double input_kP, double input_kI, double input_kD) //PIDTurn constructor. Sets kP, kI, and kD values from an input. Later input from a file?
-//         {
-//             kP = input_kP;
-//             kI = input_kI;
-//             kD = input_kD;
-//         }
-
-//         void run(float target_inches, float kI_start_at_error_value = 10, float timeout_msec = 3000) //Add settle time. This returns an array with two items: left_speed and right_speed
-//         {
-//             float sensor_input = inertial_sensor.get_rotation(); /* tracking wheel circumference*/;
-
-//             float turn_speed = basicPID(kP, kI, kD, sensor_input, target_inches, kI_start_at_error_value, timeout_msec);  
-
-//             left_drivetrain, right_drivetrain = turn_speed, -turn_speed; //sets left and right to the same speed, opposite directions as the PID outputs
-
-//         }
-
-//     };
+    };
 
 
-// class Odometry
-//     {
-//     public:
+class PIDTurn : public Controll
+    {
+    public:
 
-//         void getPostition();
-//         void moveTo(float x, float y, float orientation);
-//         void lineFollow();
+        PIDTurn(double input_kP, double input_kI, double input_kD) //PIDTurn constructor. Sets kP, kI, and kD values from an input. Later input from a file?
+        {
+            kP = input_kP;
+            kI = input_kI;
+            kD = input_kD;
+        }
 
-//     protected:
+        void run(float target_inches, float kI_start_at_error_value = 10, float timeout_msec = 3000) //Add settle time. This returns an array with two items: left_speed and right_speed
+        {
+            float sensor_input = inertial_sensor.get_rotation(); /* tracking wheel circumference*/;
 
-//     };
+            float turn_speed = basicPID(kP, kI, kD, sensor_input, target_inches, kI_start_at_error_value, timeout_msec);  
 
-// float Controll::basicPID(double kP, double kI, double kD, float sensor_input, float target, float kI_start_at_error_value, int timeout_msec, int settle_time_msec)
-//     {
-//     float error = target - sensor_input;
-//     float prev_error;
-//     float integral;
-//     float derivative;
+            left_drivetrain, right_drivetrain = turn_speed, -turn_speed; //sets left and right to the same speed, opposite directions as the PID outputs
 
-//     timer = 0;
+        }
 
-//     while(timer <= timeout_msec && timer < settle_time_msec)
-//     {
+    };
 
-//     if (error < kI_start_at_error_value)
-//         {
-//             integral += error;
-//         }
 
-//     derivative = prev_error - error;
+class Odometry
+    {
+    public:
 
-//     prev_error = error;
+        void getPostition();
+        void moveTo(float x, float y, float orientation);
+        void lineFollow();
 
-//     float output = kP*error + kI*integral + kD*derivative;
+    protected:
 
-//     if(output > 11) //if output is greater than 11 volts (out of a possible 12), cap at 11 volts
-//     {
-//         output = 11;
-//     }
+    };
 
-//     timer += 20;
-//     return output;
-//     }
-// }
+float Controll::basicPID(double kP, double kI, double kD, float sensor_input, float target, float kI_start_at_error_value, float timeout_msec)
+    {
+    float error = target - sensor_input;
+    float prev_error;
+    float integral;
+    float derivative;
 
-// Controll::Controll()
-// {
+    timer = 0;
 
-// }
+    while(timer <= timeout_msec && timer < settle_time_msec)
+    {
+
+    if (error < kI_start_at_error_value)
+        {
+            integral += error;
+        }
+
+    derivative = prev_error - error;
+
+    prev_error = error;
+
+    float output = kP*error + kI*integral + kD*derivative;
+
+    if(output > 11) //if output is greater than 11 volts (out of a possible 12), cap at 11 volts
+    {
+        output = 11;
+    }
+
+    timer += 20;
+    return output;
+    }
+}
+
+Controll::Controll()
+{
+
+}
